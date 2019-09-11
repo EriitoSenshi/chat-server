b503b947ebc93a4d7c14622b591da80904d2fa66
Hany <hanyalbouz@gmail.com>
2019-09-11 15:26:34 -0400

    started server.py

--- Server.py
+++ Server.py
@@ -1,1 +1,17 @@
 import socket
+
+HOST = ''
+PORT = 0
+buffer_size = 1024
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(HOST, PORT)
+    s.listen()
+    connection, address = s.accept()
+    with connection:
+        print('Connected by', address)
+        while True:
+            data = connection.recv(buffer_size)
+            if not data:
+                break
+            connection.sendall(data)
