282ba3bc0cd21e9631fabd051f372518348dad5b
Hany <hanyalbouz@gmail.com>
2019-09-16 13:01:06 -0400

    finished server

--- Server.py
+++ Server.py
@@ -36,20 +36,20 @@ def receive_message(client_socket):
 
 while True:
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-    '''The 3 parameters are, in order, the sockets that we want to read, the sockets that we want to write, 
-    and the sockets that we might air on'''
+    # The 3 parameters are, in order, the sockets that we want to read, the sockets that we want to write,
+    # and the sockets that we might air on
 
     for notified_socket in read_sockets:
         if notified_socket == server_socket:  # This accepts the connection from a client and handles it
-            client_socket, client_address = server_socket.accept()
+            client_connection, client_address = server_socket.accept()
 
-            user = receive_message(client_socket)
+            user = receive_message(client_connection)
             if user is False:  # If client disconnected
                 continue
 
-            sockets_list.append(client_socket)
+            sockets_list.append(client_connection)
 
-            clients[client_socket] = user
+            clients[client_connection] = user
 
             print(f"Accepted new connection from {client_address[0]}:{client_address[1]} "
                   f"username:{user['data'].decode('utf-8')}")
@@ -66,9 +66,9 @@ while True:
             user = clients[notified_socket]
             print(f"Received message from {user['data'].decode('utf-8')} : {message['data'].decode('utf-8')}")
 
-            for client_socket in clients:  # Shares the message to everyone
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+            for client in clients:  # Shares the message to everyone
+                if client != notified_socket:
+                    client.send(user['header'] + user['data'] + message['header'] + message['data'])
                     '''Sends username data and the message itself'''
 
     for notified_socket in exception_sockets:
